6799cef86cf077484699324c6e74520e86150f09
neutralboy <leopoorna99@gmail.com>
2020-03-29 00:09:44 +0530

    Updates

---
+++ precache-manifest.fb715618b5745ce1245d636d07a7d475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8087ec421226586bf70c8d734b955bf",
+    "url": "/teleprompter/index.html"
+  },
+  {
+    "revision": "56155b5f4eb87c1a819d",
+    "url": "/teleprompter/static/css/main.a195e32d.chunk.css"
+  },
+  {
+    "revision": "e5c286c1ffcd6598c430",
+    "url": "/teleprompter/static/js/2.aefbcc7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teleprompter/static/js/2.aefbcc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56155b5f4eb87c1a819d",
+    "url": "/teleprompter/static/js/main.7fa00ed2.chunk.js"
+  },
+  {
+    "revision": "0b4ce9d4092dd9968d1f",
+    "url": "/teleprompter/static/js/runtime-main.4e884d24.js"
+  }
+]);
